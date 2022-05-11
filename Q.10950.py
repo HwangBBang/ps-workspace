ea10356f09a3a60a3877d82435f6d28f4b5827ee
HwangBBang <96881651+HwangBBang@users.noreply.github.com>
2022-05-11 22:35:14 +0900

    Add files via upload

---
+++ Q.10950.py
@@ -0,0 +1,8 @@
+
+
+
+N = int(input())
+mul = 0
+for i in range (N,0,-1):
+    mul += i
+print(mul)
---
+++ Q.10952.py
@@ -0,0 +1,8 @@
+
+
+while 1:
+    a,b = map(int, input().split())
+    print(a+b)
+    k = input()
+    if k == " ":
+        break
