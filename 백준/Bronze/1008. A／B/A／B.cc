e13396cf0f450d59c9d385762b09d3de791779d7
HwangBBang <96881651+HwangBBang@users.noreply.github.com>
2022-11-14 15:12:25 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    double a;
+    double b;
+    cin >> a;
+    cin >> b;
+    cout.precision(12);
+    cout << a/b << endl;
+    
+return 0;
+}
