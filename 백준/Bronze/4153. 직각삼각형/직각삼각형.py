930ff4b672490bc0b8de18237b82a94a68c1a0ae
HwangBBang <96881651+HwangBBang@users.noreply.github.com>
2022-11-23 01:06:01 +0900

    [Bronze III] Title: 직각삼각형, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.py
@@ -0,0 +1,26 @@
+import sys
+
+input = sys.stdin.readline
+
+while True:
+    a,b,c = map(int,input().split())
+    
+    if a==0 and b==0 and c==0 :
+        break
+    
+    if max(a, b, c) == a:
+        if a**2 == b**2 + c**2:
+            print("right")
+        else:
+            print("wrong")
+            
+    elif max(a, b, c) == b:
+        if b**2 == a**2 + c**2:
+            print("right")
+        else:
+            print("wrong")
+    else:
+        if c**2 == b**2 + a**2:
+            print("right")
+        else:
+            print("wrong")
