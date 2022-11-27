7d5f04bb37bc5ffd90df1ce66e498f4effa5cc81
HwangBBang <96881651+HwangBBang@users.noreply.github.com>
2022-11-27 20:37:22 +0900

    [Bronze III] Title: 별 찍기 - 12, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.py
@@ -0,0 +1,17 @@
+n = int(input())
+
+
+for i in range(1,n+1):
+    for j in range(n-i):
+        print(" ",end = "")
+    for k in range(i):
+        print("*", end = "")
+    print()
+
+for i1 in range(1,n):
+    for j in range(i1):
+        print(" ", end="")
+    for k in range(n-i1):
+        print("*", end="")
+    print()
+
