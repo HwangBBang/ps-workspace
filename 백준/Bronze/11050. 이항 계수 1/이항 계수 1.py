b227cecdc1a93599893890cce57e258706543d3d
HwangBBang <96881651+HwangBBang@users.noreply.github.com>
2022-11-30 10:35:03 +0900

    [Bronze I] Title: 이항 계수 1, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
@@ -0,0 +1,15 @@
+n, k = map(int, input().split())
+
+
+dp = [[0 for _ in range(i+1)] for i in range(30)]
+
+dp[0][0] = 1
+
+for s in range(30):
+    dp[s][0], dp[s][s] = 1, 1
+
+for i in range(2, 30):
+    for j in range(1, i):
+        dp[i][j] = dp[i-1][j]+dp[i-1][j-1]
+
+print(dp[n][k])
