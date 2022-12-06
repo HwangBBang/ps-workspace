c8a7fa3c7ccb25a05e408b22de506ae69db0a1d1
HwangBBang <96881651+HwangBBang@users.noreply.github.com>
2022-12-06 19:11:30 +0900

    [Bronze III] Title: 홀수, Time: 32 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/2576. 홀수/홀수.py
@@ -0,0 +1,16 @@
+
+import sys
+input = sys.stdin.readline
+
+arr = []
+
+for i in range(7):
+    a = int(input())
+    if a%2==1:
+        arr.append(a)
+
+if len(arr) == 0:
+    print(-1)
+else:
+    print(sum(arr))
+    print(min(arr))
