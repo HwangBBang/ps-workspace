de6e71cb6fbb96fe93375fc1828c30a5aeb96b2b
HwangBBang <96881651+HwangBBang@users.noreply.github.com>
2022-12-15 13:53:22 +0900

    [Bronze V] Title: 학점계산, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.py
@@ -0,0 +1,34 @@
+
+# input = sys.stdin.readline
+T = input()
+
+if T == "A+" :
+    print(4.3)
+if T == "A0" :
+    print(4.0)
+if T == "A-" :
+    print(3.7)
+
+if T == "B+" :
+    print(3.3)
+if T == "B0" :
+    print(3.0)
+if T == "B-" :
+    print(2.7)
+
+if T == "C+" :
+    print(2.3)
+if T == "C0" :
+    print(2.0)
+if T == "C-" :
+    print(1.7)
+    
+if T == "D+" :
+    print(1.3)
+if T == "D0" :
+    print(1.0)
+if T == "D-" :
+    print(0.7)
+    
+if T == "F" :
+    print(0.0)
