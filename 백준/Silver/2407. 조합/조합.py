67be9cff292bda268fc9c770be78a10cbed0cac4
HwangBBang <96881651+HwangBBang@users.noreply.github.com>
2023-01-05 22:54:38 +0900

    Update 조합.py

--- 백준/Silver/2407. 조합/조합.py
+++ 백준/Silver/2407. 조합/조합.py
@@ -1,5 +1,16 @@
+# 풀이 1
 import math
-
 n,m = map(int,input().split())
-
 print(math.comb(n,m))
+
+# 풀이 2
+# result = 1
+# for i in range(n,n-m,-1):
+#     # 분자 
+#     result *= i
+
+# for i in range(m,0,-1):
+#     # 분모 
+#     result //= i
+    
+# print(result)
