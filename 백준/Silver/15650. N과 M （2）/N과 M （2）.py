e359ebab7b1037fba57c99a4d7c3e6440e7f0823
HwangBBang <96881651+HwangBBang@users.noreply.github.com>
2023-01-10 10:52:21 +0900

    [Silver III] Title: N과 M (2), Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,19 @@
+n,m = map(int,input().split())
+
+stack = []
+
+def backTrack(start):
+    
+    if len(stack) == m:
+        for elem in stack: print(elem,end=" ")
+        print()
+        return
+
+    for elem in range(start,n+1):
+        if not elem in stack:
+            stack.append(elem)
+            backTrack(elem+1)
+            stack.pop()
+            
+        
+backTrack(1)
