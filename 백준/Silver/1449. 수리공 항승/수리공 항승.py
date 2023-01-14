4305a29b0b223ee57f9cd15737ff96f860406490
HwangBBang <96881651+HwangBBang@users.noreply.github.com>
2023-01-14 17:46:36 +0900

    [Silver III] Title: 수리공 항승, Time: 32 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/1449. 수리공 항승/수리공 항승.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+n,l = map(int,input().split())
+
+spot = list(map(int,input().split()))
+
+spot.sort()
+
+# 한 개는 반듯이 붙여야해
+count = 1
+limt = (spot[0] - 0.5) + l
+
+for i in range(1, n):
+    if limt >= spot[i] + 0.5:
+        continue
+    else:
+        count += 1
+        limt = (spot[i] - 0.5) + l
+
+print(count)
