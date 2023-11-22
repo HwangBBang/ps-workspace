de053b118f2e8f83eff78368cd13ebc835404a49
HwangBBang <96881651+HwangBBang@users.noreply.github.com>
2023-11-23 00:25:32 +0900

    [level 2] Title: 전화번호 목록, Time: 236.52 ms, Memory: 39.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,18 +1,12 @@
-from collections import Counter, defaultdict
+from collections import defaultdict
 
 
 def solution(phone_book):
     answer = True
     d = defaultdict(int)
-    for idx, i in enumerate(phone_book):
-        d[i] += idx
-
-
-
-    for p in phone_book:
-        a = ''
-        for i in p:
-            a += i
-            if a in d and a != p:
-                return False
+    for i in phone_book:
+        d[i] += 1
+    for i in phone_book:
+        if i in d:
+            return False
     return answer
