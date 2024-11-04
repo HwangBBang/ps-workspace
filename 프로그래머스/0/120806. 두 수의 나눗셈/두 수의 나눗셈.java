7c8094ac4a5c8774b53e42a315c548d973324ce3
HwangBBang <h1009218@hufs.ac.kr>
2024-11-04 13:24:25 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.03 ms, Memory: 92.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -0,0 +1,7 @@
+class Solution {
+    private static final int FIX_NUM = 1000;
+    
+    public static int solution(int num1, int num2) {
+        return (int) ((double) num1 / num2 * FIX_NUM);
+    }
+}
