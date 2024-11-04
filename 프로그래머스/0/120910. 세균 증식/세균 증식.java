02f97f0d38570e9ed4dd747008767e7d69c1cad1
HwangBBang <h1009218@hufs.ac.kr>
2024-11-04 14:34:10 +0900

    [level 0] Title: 세균 증식, Time: 0.05 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/0/120910. 세균 증식/세균 증식.java
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.java
@@ -1,14 +1,17 @@
+import java.util.*;
+
 class Solution {
     public int solution(int n, int t) {
-        int answer = n;
+//         int answer = n;
         
-        for(int i = 0; i < t; i ++){
-            answer = makeTwice(answer);
-        }
-        return answer;
+//         for(int i = 0; i < t; i ++){
+//             answer = makeTwice(answer);
+//         }
+//         return answer;
+        return n * (int)Math.pow(2,t);
     }
     
-    private int makeTwice(int n){
-        return n * 2;
-    }
+    // private int makeTwice(int n){
+    //     return n * 2;
+    // }
 }
