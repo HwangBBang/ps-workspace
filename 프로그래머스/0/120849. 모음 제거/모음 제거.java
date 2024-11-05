7cdf9134fe712256531e4f41e02a2624cd533e64
BYEONGHUN HWANG <h1009218@hufs.ac.kr>
2024-11-05 18:32:48 +0900

    [level 0] Title: 모음 제거, Time: 0.15 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -1,9 +1,12 @@
 class Solution {
     public String solution(String my_string) {
-        String[] targets = {"a", "e", "i", "o", "u"};
-        for (String target : targets) {
-            my_string = my_string.replace(target,"");
-        }
+        // String[] targets = {"a", "e", "i", "o", "u"};
+        // for (String target : targets) {
+        //     my_string = my_string.replace(target,"");
+        // }
+        
+        my_string = my_string.replaceAll("[aeiou]","");
+//        my_string = my_string.replaceAll("a|e|i|o|u","");
         return my_string;
     }
 }
