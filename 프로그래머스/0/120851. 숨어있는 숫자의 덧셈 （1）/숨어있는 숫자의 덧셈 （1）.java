291bbffb6ab7f371aaf1d92be23a4667fb95e072
BYEONGHUN HWANG <h1009218@hufs.ac.kr>
2024-11-05 16:53:58 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.35 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -2,8 +2,10 @@ class Solution {
     public int solution(String my_string) {
         int answer = 0;
         String[] str = my_string.split("");
+        
+        String compareStr = "0123456789";
         for (String s : str){
-            if(s.matches("[0-9]")){
+            if(compareStr.contains(s)){
                 answer += Integer.parseInt(s);
             }   
         }
