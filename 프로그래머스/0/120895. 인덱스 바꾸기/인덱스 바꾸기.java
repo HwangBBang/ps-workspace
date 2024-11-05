0d3c0d56648d6329026f95de5c70391d6c5fe762
BYEONGHUN HWANG <h1009218@hufs.ac.kr>
2024-11-06 00:07:39 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.19 ms, Memory: 82.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        String[] str = my_string.split("");
+        
+        String remNum1 = str[num1];
+        str[num1] = str[num2];
+        str[num2] = remNum1;
+        
+        return String.join("", str);
+    }
+}
