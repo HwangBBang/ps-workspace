0a86a45cf3b37412d6aea56f882d0df5b64fa4de
BYEONGHUN HWANG <h1009218@hufs.ac.kr>
2024-11-05 23:49:29 +0900

    [level 0] Title: 숫자 찾기, Time: 0.05 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -1,17 +1,20 @@
 class Solution {
     public int solution(int num, int k) {
-        int answer = 0;
-        String[] nums = String.valueOf(num).split("");
+        // int answer = 0;
+//         String[] nums = String.valueOf(num).split("");
         
-        for(String s : nums){
-            answer ++; 
+//         for(String s : nums){
+//             answer ++; 
             
-            if (Integer.parseInt(s) == k){
-                return answer;
-            }
+//             if (Integer.parseInt(s) == k){
+//                 return answer;
+//             }
+//         }
+        int idx = String.valueOf(num).indexOf(String.valueOf(k));
+        if (idx == -1) {
+            return idx;
         }
-        
-        
-        return -1;
+        return idx + 1;
+    
     }
 }
