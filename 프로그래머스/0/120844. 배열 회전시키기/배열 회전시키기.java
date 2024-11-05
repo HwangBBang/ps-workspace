5fa4eade603a090af49b1761dd36fd4322c6080e
BYEONGHUN HWANG <h1009218@hufs.ac.kr>
2024-11-05 23:30:20 +0900

    [level 0] Title: 배열 회전시키기, Time: 5.42 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.java
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.java
@@ -1,22 +1,34 @@
+import java.util.*;
+import java.util.stream.*;
 class Solution {
     public int[] solution(int[] numbers, String direction) {
         int len = numbers.length;
-        int[] result = new int[len];
+//         int[] result = new int[len];
 
-        if (direction.equals("right")){
-            result[0] = numbers[len - 1];
-            for (int i = 1; i < len; i++) {
-                result[i] = numbers[i - 1];
-            }   
-        }
-        else if(direction.equals("left")){
-            result[len - 1] = numbers[0];
-            for (int i = 1; i < len; i++) {
-                result[i-1] = numbers[i];
-            }
+//         if (direction.equals("right")){
+//             result[0] = numbers[len - 1];
+//             for (int i = 1; i < len; i++) {
+//                 result[i] = numbers[i - 1];
+//             }   
+//         }
+//         else if(direction.equals("left")){
+//             result[len - 1] = numbers[0];
+//             for (int i = 1; i < len; i++) {
+//                 result[i-1] = numbers[i];
+//             }
             
+//         }
+//         return result;
+        List<Integer> list = Arrays.stream(numbers).boxed().collect(Collectors.toList());
+
+        if (direction.equals("right")) {
+            list.add(0, list.get(len - 1));
+            list.remove(len);
+        } else if (direction.equals("left")) {
+            list.add(list.get(0));
+            list.remove(0);
         }
-        return result;
+        return list.stream().mapToInt(Integer::intValue).toArray();
     }
 }
 
