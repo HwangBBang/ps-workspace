de408a56eb2e9688b4a4ac7e5e1db89d2af621a2
BYEONGHUN HWANG <h1009218@hufs.ac.kr>
2024-11-05 18:52:14 +0900

    [level 0] Title: 배열 자르기, Time: 3.44 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
@@ -1,9 +1,11 @@
 import java.util.*;
+import java.util.stream.*;
 
 class Solution {
     public int[] solution(int[] numbers, int num1, int num2) {
 
-        int[] answer = Arrays.copyOfRange(numbers, num1, num2+1);
+        // int[] answer = Arrays.copyOfRange(numbers, num1, num2+1);
+        int[] answer = IntStream.rangeClosed(num1,num2).map(num -> numbers[num]).toArray();
         return answer;
     }
 }
