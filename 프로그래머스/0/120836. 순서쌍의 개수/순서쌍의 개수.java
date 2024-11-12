18ffb3165f0e68c0e98c760d85aa42128909225f
BYEONGHUN HWANG <h1009218@hufs.ac.kr>
2024-11-12 15:24:24 +0900

    [level 0] Title: 순서쌍의 개수, Time: 27.30 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.java
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.java
@@ -1,12 +1,6 @@
-import java.util.stream.*;
-import java.util.*;
-
 class Solution {
     public int solution(int n) {
-        int answer = 0;        
-        int[] divisors = IntStream.rangeClosed(1, n).filter(i -> n % i == 0).toArray();
-                
-        
-        return divisors.length;
+        int answer = 0;
+        return answer;
     }
 }
