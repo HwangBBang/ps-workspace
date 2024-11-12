c2bf6345f50af177341372060cae1acfd904a70d
BYEONGHUN HWANG <h1009218@hufs.ac.kr>
2024-11-12 16:40:45 +0900

    [level 0] Title: 중복된 문자 제거, Time: 1.22 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.java
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.java
@@ -2,15 +2,14 @@ import java.util.*;
 
 class Solution {
     public String solution(String my_string) {
-        StringBuilder sb = new StringBuilder();
+        
         //      문자열을 문자열 리스트로 만든다.
         List<String> strings = List.of(my_string.split(""));
 
         //      문자열을 순서 집합으로 만든다.
-        LinkedHashSet<String> answer = new LinkedHashSet<>(strings);
-        for (String str : answer) {
-            sb.append(str);
-        }
-        return sb.toString();
+        LinkedHashSet<String> stringSet = new LinkedHashSet<>(strings);
+        
+        String answer = String.join("",stringSet);
+        return answer;
     }
 }
