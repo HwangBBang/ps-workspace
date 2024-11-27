5e4607d1f013c2927e98ae83c78836a7418f9785
BYEONGHUN HWANG <h1009218@hufs.ac.kr>
2024-11-27 23:42:23 +0900

    [level 0] Title: 뒤에서 5등까지, Time: 0.46 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.java
+++ 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.java
@@ -2,11 +2,9 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] num_list) {
-        int[] answer = new int[5];
-        int[] array1 = Arrays.stream(num_list).sorted().toArray();
-        for (int i = 0; i < answer.length; i++) {
-            answer[i] = array1[i];
-        }
+     
+        Arrays.sort(num_list);
+        int[] answer = Arrays.copyOfRange(num_list, 0, 5);
 
         return answer;
     }
