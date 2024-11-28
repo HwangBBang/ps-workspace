3d4cf75d72988701faf9e19060eb207708e778b5
BYEONGHUN HWANG <h1009218@hufs.ac.kr>
2024-11-29 00:36:01 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 1.27 ms, Memory: 85.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        int len = num_list.length;
+        Arrays.sort(num_list);
+        
+        int[] answer = Arrays.copyOfRange(num_list, 5, len);
+        return answer;
+    }
+}
