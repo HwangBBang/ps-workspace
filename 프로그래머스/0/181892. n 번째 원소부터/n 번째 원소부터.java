dc50a930f637fa0528212fa12ff48766b7bf3b71
BYEONGHUN HWANG <h1009218@hufs.ac.kr>
2024-11-28 22:57:45 +0900

    [level 0] Title: n 번째 원소부터, Time: 0.03 ms, Memory: 98.2 MB -BaekjoonHub

--- 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.java
+++ 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.java
@@ -3,13 +3,7 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] num_list, int n) {
         int len = num_list.length;
-        ArrayList<Integer> answer = new ArrayList();
-        
-        for (int i = n-1; i < len; i ++){
-            answer.add(num_list[i]);
-        }
-        
-        
-        return answer.stream().mapToInt(Integer::intValue).toArray();
+        return Arrays.copyOfRange(num_list,n-1, len);
+    
     }
 }
