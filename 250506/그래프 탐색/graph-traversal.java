7d780433c651bbecb796370536acff3df031f25f
BYEONGHUN HWANG <h1009218@hufs.ac.kr>
2025-05-06 12:33:55 +0900

    Title: 그래프 탐색, Time: 195ms, Memory: 17MB, Status: Passed - Codetree

--- 250506/그래프 탐색/graph-traversal.java
+++ 250506/그래프 탐색/graph-traversal.java
@@ -1,15 +1,53 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.StringTokenizer;
+import java.util.*;
+
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int[] u = new int[m];
-        int[] v = new int[m];
+    static int n, m;
+    static int[][] grid;
+    static boolean[] visited;
+    static List<Integer> answer = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        grid = new int[n][n];
+        visited = new boolean[n];
+
         for (int i = 0; i < m; i++) {
-            u[i] = sc.nextInt();
-            v[i] = sc.nextInt();
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken()) - 1;
+            int y = Integer.parseInt(st.nextToken()) - 1;
+            grid[x][y] = 1;
+            grid[y][x] = 1;
         }
-        // Please write your code here.
+
+        visited[0] = true;
+        dfs(0);
+
+        System.out.println(answer.size());
+
+    }
+
+    static void dfs(int start) {
+        for (int i = 0; i < n; i++) {
+            if (!visited[i] && grid[start][i] == 1) {
+                visited[i] = true;
+                answer.add(i);
+                dfs(i);
+
+            }
+        }
     }
 }
+
+//1 2
