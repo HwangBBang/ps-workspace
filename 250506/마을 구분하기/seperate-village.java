3beaabde800f8bd85bffd3bcc78a29dceb5dce15
BYEONGHUN HWANG <h1009218@hufs.ac.kr>
2025-05-06 15:21:55 +0900

    Title: 마을 구분하기, Time: 104ms, Memory: 9MB, Status: Passed - Codetree

--- 250506/마을 구분하기/seperate-village.java
+++ 250506/마을 구분하기/seperate-village.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class DFS3 {
+public class Main {
     static int n;
     static int[][] grid;
     static boolean[][] visited;
