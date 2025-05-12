2c6d0075587a68b8c33ea4714232e3c98ed71c16
BYEONGHUN HWANG <h1009218@hufs.ac.kr>
2025-05-12 17:38:15 +0900

    [level 2] Title: 조이스틱, Time: 0.06 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -1,36 +1,3 @@
-// class Solution {
-//     public int solution(String name) {
-//         int answer1 = 0;
-//         int answer2 = 0;
-        
-// //         char[] charSet = new char[26];
-        
-// //         for (int i = 0; i < 26; i ++){
-// //             charSet[i] = (char)('A' + i);    
-// //         }
-        
-//         char[] dataSet = name.toCharArray();
-//         int[] difSet = new int[dataSet.length];
-        
-//         for (int i = 0; i < dataSet.length; i++){   
-//             int left = dataSet[i] - 'A';
-//             int right = 26 + ('A' - dataSet[i]);
-//             difSet[i] = Math.min(left,right);
-//         }
-        
-//         for (int i = 0; i < difSet; i++){   
-//             // 순방향 순회 
-//             answer1 = difSet[i] + 1;
-            
-//             // 역방향 순회 
-//             answer2 = difSet[difSet.length-i] + 1;
-                
-//         }
-    
-//         return answer;
-//     }
-// }
-
 class Solution {
     public int solution(String name) {
         
@@ -55,10 +22,10 @@ class Solution {
             while (next < n && name.charAt(next) == 'A') {
                 next++;
             }
-            // 경우 1: i → 0 → (n-1→next)
-            move = Math.min(move, 2 * i + (n - next));
-            // 경우 2: (n-1 → next) → 0→i
-            move = Math.min(move, i + 2 * (n - next));
+            // (1) 오른쪽으로 갔다가 복귀 후, 왼쪽에서 건너뛰기
+            move = Math.min(move, i  +  i  +  (n - next));
+            // (2) 왼쪽(A 구간 뒤쪽) 먼저 건너뛰기
+            move = Math.min(move, (n - next) + (n - next) + i );
         }
     
         return answer + move;
