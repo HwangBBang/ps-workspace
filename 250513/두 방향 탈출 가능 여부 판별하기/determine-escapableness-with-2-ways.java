22ebb84cee6a56880c6241a7539d840edb557130
BYEONGHUN HWANG <h1009218@hufs.ac.kr>
2025-05-13 17:31:51 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250513/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.java
@@ -0,0 +1,126 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n, m;
+    static int[][] grid;
+    static boolean[][] visited;
+    static int answer = 0;
+
+    static int[] dx = {0, 1};
+    static int[] dy = {1, 0};
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        grid = new int[n][m];
+        visited = new boolean[n][m];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int ii = 0; ii < m; ii++) {
+                grid[i][ii] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        visited[0][0] = true;
+        dfs(0,0);
+
+
+        System.out.println(answer);
+
+    }
+
+    static void dfs(int x, int y){
+        if(x == n-1 && y == m-1) {
+            answer = 1;
+            return;
+        }
+
+        for (int i = 0; i < dx.length; i ++){
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (isNotRange(nx,ny)) continue;
+            if (visited[nx][ny]) continue;
+            if (grid[nx][ny] == 0) continue;
+            
+            visited[nx][ny] = true;
+            dfs(nx,ny);
+        }
+    }
+
+    static boolean isNotRange(int x, int y){
+        return x < 0 || x >= n || y < 0 || y >= m;
+    }
+}
+
+//1 2
+
+// public class Main {
+//     static int n, m;
+//     static int[][] grid;
+//     static boolean[][] visited;
+//     static int answer = 0;
+
+//     static int[] dx = {0, 1};
+//     static int[] dy = {1, 0};
+
+//     public static void main(String[] args) throws IOException {
+
+//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//         StringTokenizer st;
+//         st = new StringTokenizer(br.readLine());
+
+//         n = Integer.parseInt(st.nextToken());
+//         m = Integer.parseInt(st.nextToken());
+
+//         grid = new int[n][m];
+//         visited = new boolean[n][m];
+
+//         for (int i = 0; i < n; i++) {
+//             st = new StringTokenizer(br.readLine());
+//             for (int ii = 0; ii < m; ii++) {
+//                 grid[i][ii] = Integer.parseInt(st.nextToken());
+//             }
+//         }
+
+//         visited[0][0] = true;
+//         dfs(0, 0);
+
+//         System.out.println(answer);
+
+//     }
+
+//     static void dfs(int x, int y) {
+//         if (x == n-1 && y == m-1) {
+//             answer = 1;
+//             return;
+//         }
+
+//         for (int i = 0; i < 2; i++) {
+//             int nx = x + dx[i];
+//             int ny = y + dy[i];
+
+//             if(isNotRange(nx, ny)) continue;
+//             if (visited[nx][ny]) continue;
+//             if (grid[nx][ny] == 0) continue;
+
+//             visited[nx][ny] = true;
+//             dfs(nx, ny);
+//         }
+//     }
+//     static boolean isNotRange(int x, int y) {
+//         return x < 0 || x >= n || y < 0 || y >= m;
+//     }
+// }
+
+// //1 2
