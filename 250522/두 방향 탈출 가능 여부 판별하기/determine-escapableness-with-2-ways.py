322d4c8b616b9cc874833a3d2f5b59bceb414efc
BYEONGHUN HWANG <h1009218@hufs.ac.kr>
2025-05-22 21:23:59 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 1067ms, Memory: 27MB, Status: Time Limit Exceed - Codetree

--- 250522/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
+++ 250522/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -1,43 +1,43 @@
-def rangeOut(nextRow,nextColumn):
+def rangeOut(nextRow, nextColumn):
     global n
     global m
-    return nextRow < 0 or nextRow >= n or nextColumn < 0 or nextColumn >= m # true 
-    
+    return nextRow < 0 or nextRow >= n or nextColumn < 0 or nextColumn >= m  # true
 
 
-def sol(r,c):    
+def sol(r, c):
     global n
     global m
     global result
     if r == n-1 and c == m-1:
         result = 1
         return
-    
-    for i in range(2):
-        nR = r+dR[i]
-        nC = c+dC[i]
-        
-        if rangeOut(nR,nC): continue
-        if grid[nR][nC] == 0: continue
-        
-        sol(nC,nR)
 
+    for i in range(2):
+        nR = r + dR[i]
+        nC = c + dC[i]
+
+        if rangeOut(nR, nC):
+            continue
+        if grid[nR][nC] == 0:
+            continue
+
+        sol(nR, nC)
 
 
 global n
 global m
 global result
 
-dR=[1,0]
-dC=[0,1]
+dR = [1, 0]
+dC = [0, 1]
 result = 0
 
-n,m = map(int,input().split())
+n, m = map(int, input().split())
 
 grid = []
 for i in range(n):
-    grid.append(list(map(int,input().split())))
+    grid.append(list(map(int, input().split())))
 
 
-sol(0,0)
+sol(0, 0)
 print(result)
