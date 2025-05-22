ad677c7b660e27e09e55e4bdf7afae39d0fb478f
BYEONGHUN HWANG <h1009218@hufs.ac.kr>
2025-05-22 21:28:53 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 76ms, Memory: 19MB, Status: Passed - Codetree

--- 250522/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
+++ 250522/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -1,3 +1,86 @@
+
+
+# for i in range(1,41,1):
+
+#  순 회  돌아라  뭐를? iterable object
+
+# for i in range()
+# 반복가능한 객체 : range()
+
+# 반복가능한 객체
+# 반복가능한 객체는 뭐에요? 어떻게 구분하죠?
+# 반복가능한 객체 : 반복가능한 (객체 == 어떤 것)
+
+
+# str
+#         iterable
+# 1         | X | 객체 int
+# "@31"     | O | 객체 str
+# "1"       | O | 객체 str
+# 43141.31  | X | 객체 float
+# False     | X | 객체 bool
+# [1]       | O | 객체 list
+# [1,1,1]   | O | 객체 list
+# range()   | O | 객체 range
+
+
+# 배 열
+# 문자 열
+
+# 기본형..
+# 참조형..
+
+# 판단 근거..?
+# int, bool, float
+
+
+# print(type(range(0)))
+# print(type([1, 414, 3]))
+# for i in "1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1":
+# print(i)
+
+# for
+
+
+# 별로 없엉,, for 개좋아 ! 컨트롤이 쉬워요
+# 기말에 어려워
+
+# while
+
+# 쌤 이번에 차샀어
+# => 무슨 차요?
+
+# 영화  "총"이 나와
+# 반드시 ... 쏴야해
+
+#  별표 찍기 1.
+
+# n = 4
+# *
+# * *
+# * * *
+# * * * *
+
+
+# n = 6
+# *
+# * *
+# * * *
+# * * * *
+# * * * * *
+# * * * * * *
+
+# 반복해서 출력
+
+#  n 을 받음 ..
+# n 의 의미?
+
+# n = int(input())
+
+# for i in range(1, n+1):  # 행
+#     print("* " * i)
+
+# ==============
 def rangeOut(nextRow, nextColumn):
     global n
     global m
@@ -13,15 +96,20 @@ def sol(r, c):
         return
 
     for i in range(2):
-        nR = r + dR[i]
-        nC = c + dC[i]
+        nr = r + dR[i]
+        nc = c + dC[i]
 
-        if rangeOut(nR, nC):
-            continue
-        if grid[nR][nC] == 0:
+        if rangeOut(nr, nc):
             continue
 
-        sol(nR, nC)
+        if v[nr][nc]:
+            continue
+
+        if grid[nr][nc] == 0:
+            continue
+
+        v[nr][nc] = True
+        sol(nr, nc)
 
 
 global n
@@ -30,10 +118,12 @@ global result
 
 dR = [1, 0]
 dC = [0, 1]
+
 result = 0
 
 n, m = map(int, input().split())
 
+v = [[False for _ in range(m)] for _ in range(n)]
 grid = []
 for i in range(n):
     grid.append(list(map(int, input().split())))
