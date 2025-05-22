9712c98347f1eb177eb8bd5b3f331859b4973494
BYEONGHUN HWANG <h1009218@hufs.ac.kr>
2025-05-22 21:16:22 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250522/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
+++ 250522/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -1,44 +1,39 @@
-def rangeOut(nextRow,nextColumn,N,M):
-    if nextRow < 0 or nextRow >= N or nextColumn < 0 or nextColumn >= M:
-        return True
-    return False
-def end(cC,cR,r,c):
-    if cC==c-1 and cR==r-1:
-        return True
-    return False
+def rangeOut(nextRow,nextColumn):
+    return nextRow < 0 or nextRow >= n or nextColumn < 0 or nextColumn >= m # true 
+    
 
-def sol(currentC,currentR,row,column,grid):
-    dR=[1,0]
-    dC=[0,1]
-    if end(currentC,currentR,row,column):
+
+def sol(r,c):    
+    global result
+    if r == n-1 and c == m-1:
+        result = 1
         return
+    
     for i in range(2):
-        nR=currentR+dR[i]
-        nC=currentC+dC[i]
+        nR = r+dR[i]
+        nC = c+dC[i]
         
-        if rangeOut(nR,nC,row,column):
-            continue
-        if grid[nR][nC] != 1:
-            continue
+        if rangeOut(nR,nC): continue
+        if grid[nR][nC] == 0: continue
         
-        
-        grid[nR][nC]="X"
-        # print(grid)
-        sol(nC,nR,row,column,grid)
+        sol(nC,nR)
 
 
 
+global n
+global m
+global result
 
-N,M=map(int,input().split())
-grid=[[0] for _ in range(M)]
-# print(grid)
-for i in range(N):
-    grid[i]=list(map(int,input().split()))
-    # print(grid)
-currentR=0
-currentC=0
-sol(currentC,currentR,N,M,grid)
-if grid[N-1][M-1]==1:
-    print(0)
-else:
-    print(1)
+dR=[1,0]
+dC=[0,1]
+result = 0
+
+n,m = map(int,input().split())
+
+grid = []
+for i in range(n):
+    grid.append(list(map(int,input().split())))
+
+
+sol(0,0)
+print(result)
