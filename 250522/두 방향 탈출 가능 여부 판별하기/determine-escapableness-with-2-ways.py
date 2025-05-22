c8b4870333804726cdeb7d863073de730a685797
BYEONGHUN HWANG <h1009218@hufs.ac.kr>
2025-05-22 20:26:32 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 76ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 250522/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -0,0 +1,44 @@
+def rangeOut(row,column,N,M):
+    if row < 0 or row >= N or column < 0 or column >= M:
+        return True
+    return False
+def end(cC,cR,r,c):
+    if cC==c-1 and cR==r-1:
+        return True
+    return False
+
+def sol(currentC,currentR,row,column,grid):
+    dR=[1,0]
+    dC=[0,1]
+    if end(currentC,currentR,row,column):
+        return
+    for i in range(2):
+        nR=currentR+dR[i]
+        nC=currentC+dC[i]
+        
+        if rangeOut(nR,nC,row,column):
+            continue
+        if grid[nR][nC] != 1:
+            continue
+        
+        
+        grid[nR][nC]="X"
+        # print(grid)
+        sol(nC,nR,row,column,grid)
+
+
+
+
+N,M=map(int,input().split())
+grid=[[0] for _ in range(N)]
+# print(grid)
+for i in range(M):
+    grid[i]=list(map(int,input().split()))
+    # print(grid)
+currentR=0
+currentC=0
+sol(currentC,currentR,N,M,grid)
+if grid[M-1][N-1]==1:
+    print(0)
+else:
+    print(1)
