4f8c73fcf6a57e5e547ae46af3b9f4f8f59b0ba2
BYEONGHUN HWANG <h1009218@hufs.ac.kr>
2025-06-28 01:21:50 +0900

    Title: 사각형 채우기 2, Time: 126ms, Memory: 10MB, Status: Passed - Codetree

--- 250628/사각형 채우기 2/rectangle-fill-2.java
+++ 250628/사각형 채우기 2/rectangle-fill-2.java
@@ -11,7 +11,7 @@ public class Main {
         dp[2] = 1 + dp[1] + 1;
 
         for (int i = 3; i <= n ; i ++){
-            dp[i] = (2*dp[i-2])%10007 + (dp[i-1])%10007;
+            dp[i] = ((2*dp[i-2]) % 10007 + (dp[i-1])%10007) % 10007;
         }
 
         System.out.println(dp[n]);
