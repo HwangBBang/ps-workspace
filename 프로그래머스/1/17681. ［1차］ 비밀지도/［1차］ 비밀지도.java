31776dd8a8e0f01750d2873213c17fc61fa156ac
BYEONGHUN HWANG <h1009218@hufs.ac.kr>
2025-07-29 00:49:52 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.33 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,42 +1,6 @@
-import java.util.*;
-
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] answer = new String[n];
-        
-        for (int i = 0; i < n; i ++){
-            String line1 = convertBinary(arr1[i], n);
-            String line2 = convertBinary(arr2[i], n);
-            
-            String resultLine = or(line1, line2);
-            answer[i] = resultLine;
-        }
-        
+        String[] answer = {};
         return answer;
     }
-    
-    static String convertBinary(int decimal, int n){
-        StringBuilder sb = new StringBuilder();
-        String original = Integer.toBinaryString(decimal);
-
-        for (int i = 0; i < n - original.length(); i++){
-            sb.append("0");    
-        }
-        sb.append(original);
-        
-        return sb.toString();
-    }
-    
-    static String or(String b1, String b2){
-        StringBuilder sb = new StringBuilder();
-        for (int i = 1; i <= b1.length(); i ++ ){
-            String each1 = b1.substring(i-1,i);
-            String each2 = b2.substring(i-1,i);
-            
-            if (each1.equals("1")|| each2.equals("1")) sb.append("#");
-            else sb.append(" ");
-        }
-        
-        return sb.toString();
-    }   
 }
