62e210a78df670c1aad4745de6a5219d958eb327
BYEONGHUN HWANG <h1009218@hufs.ac.kr>
2025-09-23 00:15:10 +0900

    Title: 배열 추출, Time: 1274ms, Memory: 56MB, Status: Wrong Answer - Codetree

--- 250923/배열 추출/array-extraction.java
+++ 250923/배열 추출/array-extraction.java
@@ -3,25 +3,27 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>(5, (a,b)-> Integer.compare(b,a));
+        PriorityQueue<Integer> pq = new PriorityQueue<>(100_000, (a,b)-> Integer.compare(b,a));
 
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         for (int i = 0; i < n; i++) {
             int x = sc.nextInt();
+
             if (x != 0){
-                if (pq.size() < 5) pq.add(x);
+                if (pq.size() < 100_000) pq.add(x);
                 else {
                     if (pq.peek() > x) continue;
                     else pq.add(x);
                 }
                 continue;
             }
-            int result = 0;
-            if (!pq.isEmpty()) {
-                result = pq.poll();
+            else {
+                int result = 0;
+                if (!pq.isEmpty()) result = pq.poll();
+            
+                System.out.println(result);
             }
-            System.out.println(result);
 
         }
     }
