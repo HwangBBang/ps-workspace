9f48d6c9d34c600c9b1fbce0044efb852407030a
BYEONGHUN HWANG <h1009218@hufs.ac.kr>
2025-09-22 20:51:47 +0900

    Title: 최대로 연속한 숫자, Time: 4092ms, Memory: 27MB, Status: Time Limit Exceed - Codetree

---
+++ 250922/최대로 연속한 숫자/maximum-consecutive-number.java
@@ -0,0 +1,69 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int m = sc.nextInt();
+        int[] removals = new int[m];
+
+        TreeSet<Integer> tree = new TreeSet<>();
+
+        for (int i = 0; i < m; i++) {
+            tree.add(sc.nextInt());
+
+            int result = solution(tree, n);
+            
+            System.out.println(result);
+        }
+
+    }
+
+    static int solution(TreeSet<Integer> tree, int n){
+        if (tree.isEmpty()) return n + 1;
+
+        int maxLen = 0;
+        Integer first = tree.first();
+        
+        // 첫 구간 
+        if(first > 0){
+            maxLen = Math.max(maxLen, first);
+        }
+        // 중간 구간들 
+        Integer prev = null;
+        for (Integer curr : tree){
+            if (prev != null){
+                int eachLen = curr - prev - 1;
+                maxLen = Math.max(maxLen, eachLen);
+            }
+            prev = curr;
+        }
+
+        // 마지막 구간 
+        Integer last = tree.last();
+        if(last < n){
+            maxLen = Math.max(maxLen, n - last);
+        }
+        return maxLen;
+    }
+}
+/*
+
+    N이 최대 10^9라서 전체 배열을 들고 있는 건 불가. 매번 선형으로 훑는 것도 불가.
+    
+    지워진 수들의 위치에 의해서만 결정 -> 최장 구간 결정 
+    0 1 2 3 4 5 6 7 8 / 8
+
+    0 1 2   4 5 6 7 8 / 5
+    3을 끊어 버리면, 3과 연결된 2,4 서브셋을 순회해서 서브셋 크기 카운트 ??
+
+    0 1 2   4 5   7 8 / 3
+
+    0 1     4 5   7 8 / 2
+
+    [0] 2 3 6 [n] 끊어진 부분 
+
+    길이 n (8)
+    
+*/
