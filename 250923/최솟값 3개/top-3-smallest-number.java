79550eaa2efbd017d970ff6d2a9a400f1b2e551f
BYEONGHUN HWANG <h1009218@hufs.ac.kr>
2025-09-23 01:51:08 +0900

    Title: 최솟값 3개, Time: 286ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250923/최솟값 3개/top-3-smallest-number.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(3,
+        (a,b) -> Integer.compare(b,a));
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+
+        int answer ;
+        for (int i = 0; i < N; i++) {
+            answer = 1;
+            arr[i] = sc.nextInt();
+            
+            if (pq.size() < 3) pq.add(arr[i]);
+            else {
+                if (arr[i] < pq.peek()){
+                    pq.poll(); 
+                    pq.add(arr[i]);
+                }
+            }
+            if (pq.size() < 3){
+                System.out.println(answer * -1);
+            }else{
+                for (Integer elem : pq) answer *= elem;
+                System.out.println(answer);
+            }
+            
+        }
+    }
+}
