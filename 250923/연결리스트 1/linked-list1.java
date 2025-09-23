1c1f7d975f12da32b06f39b917cdb73efd176d19
BYEONGHUN HWANG <h1009218@hufs.ac.kr>
2025-09-23 19:32:54 +0900

    Title: 연결리스트 1, Time: 91ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 250923/연결리스트 1/linked-list1.java
@@ -0,0 +1,80 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static class Node{
+        String data;
+        Node prev;
+        Node next; 
+        public Node(String data){
+            this.data = data;
+            this.prev = null;
+            this.next = null;
+        }
+
+        public void setPrev(String data){
+            this.prev = new Node(data);
+            this.prev.next = this;
+        }
+        public void setNext(String data){
+            this.next = new Node(data);
+            this.next.prev = this;
+        }
+        public void changeToPrev(){
+            if (this.prev != null){
+                this.next = this;
+                this.data = this.prev.data;
+            }
+        }
+        public void changeToNext(){
+            if (this.next != null){
+                this.prev = this;
+                this.data = this.next.data;
+            }
+        }
+
+        @Override
+        public String toString(){
+            String prevS = this.prev == null ? "(Null)" : this.prev.data;
+            String nextS = this.next == null ? "(Null)" : this.next.data;
+            
+            return prevS + " " + this.data + " " + nextS;
+        }
+    }
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringTokenizer st;
+                 
+        st = new StringTokenizer(br.readLine());
+        String cur = st.nextToken();
+        Node curNode = new Node (cur);
+
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < n; i ++){
+            st = new StringTokenizer(br.readLine());
+
+            int cmd = Integer.parseInt(st.nextToken());
+            
+            if (cmd == 1){
+                String data = st.nextToken();
+                curNode.setPrev(data);
+                System.out.println(curNode.toString());
+            }
+            else if (cmd == 2){
+                String data = st.nextToken();
+                curNode.setNext(data);
+                System.out.println(curNode.toString());
+            }
+            else if (cmd == 3){
+                
+            }
+            else if (cmd == 4){
+
+            }
+        }
+    }
+}
