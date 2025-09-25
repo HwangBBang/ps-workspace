697ded533d0f795ba99b1e6bb310b6a55dc459cc
BYEONGHUN HWANG <h1009218@hufs.ac.kr>
2025-09-25 16:34:13 +0900

    Title: 부분 수열의 합이 K, Time: 120ms, Memory: 9MB, Status: Passed - Codetree

--- 250925/부분 수열의 합이 K/the-sum-of-the-subsequences-is-k.java
+++ 250925/부분 수열의 합이 K/the-sum-of-the-subsequences-is-k.java
@@ -1,14 +1,26 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
+
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int k = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        // Scanner sc = new Scanner(System.in);
+        // int n = sc.nextInt();
+        // int k = sc.nextInt();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st; 
+        
+        st = new StringTokenizer(br.readLine());
+        
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
         int[] arr = new int[n+1];
         int[] prefixSum = new int[n+1];
-
+        
+        st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= n; i++) {
-            arr[i] = sc.nextInt();
+            arr[i] = Integer.parseInt(st.nextToken());
             prefixSum[i] = prefixSum[i-1] + arr[i];
         }
         int result = 0;
