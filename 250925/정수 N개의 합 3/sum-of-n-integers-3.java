4f5c590920b38dc972fe4325d26c7b9b663d3003
BYEONGHUN HWANG <h1009218@hufs.ac.kr>
2025-09-25 16:13:11 +0900

    Title: 정수 N개의 합 3, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250925/정수 N개의 합 3/sum-of-n-integers-3.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[][] arr = new int[n+1][n+1];
+        int[][] prefixSum = new int[n+1][n+1];
+
+        int result = Integer.MIN_VALUE;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                arr[i][j] = sc.nextInt();
+                prefixSum[i][j] = prefixSum[i][j-1] + prefixSum[i-1][j] - prefixSum[i-1][j-1] + arr[i][j];
+                result = Math.min(result, prefixSum);
+            }
+        }
+
+        System.out.println(result);
+        
+    }
+}
