07bd1a2749ac1c158fd956ad282d4c38025e17d8
BYEONGHUN HWANG <h1009218@hufs.ac.kr>
2025-09-26 16:27:09 +0900

    Title: 부분수열 여부 판단하기, Time: 217ms, Memory: 17MB, Status: Passed - Codetree

--- 250926/부분수열 여부 판단하기/determine-subsequence.java
+++ 250926/부분수열 여부 판단하기/determine-subsequence.java
@@ -1,17 +1,26 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st; 
+        
+        st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
         int[] A = new int[n];
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            A[i] = sc.nextInt();
+            A[i] = Integer.parseInt(st.nextToken());
         }
         int[] B = new int[m];
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < m; i++) {
-            B[i] = sc.nextInt();
+            B[i] = Integer.parseInt(st.nextToken());
         }
         String answer = isSub(A,B) ? "Yes" : "No";
 
