7c88efe4df2429018de643a1da7e57e44a04c6fb
BYEONGHUN HWANG <h1009218@hufs.ac.kr>
2025-09-26 14:30:53 +0900

    Title: 점 개수 세기, Time: 2066ms, Memory: 63MB, Status: Passed - Codetree

--- 250926/점 개수 세기/count-number-of-points.java
+++ 250926/점 개수 세기/count-number-of-points.java
@@ -1,16 +1,32 @@
 import java.util.*;
+import java.io.*;
+
+
 public class Main {
     static int zipSize;
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st; 
+
         TreeSet<Integer> tree = new TreeSet<>();
         HashMap<Integer, Integer> mapper = new HashMap<>();
-        int N = sc.nextInt();
-        int Q = sc.nextInt();
+        st = new StringTokenizer(br.readLine());
+
+        
+        int N = Integer.parseInt(st.nextToken());
+        int Q = Integer.parseInt(st.nextToken());
+
         int[] points = new int[N];
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            points[i] = sc.nextInt();
+            points[i] = Integer.parseInt(st.nextToken());
             tree.add(points[i]);
+        }
+        int[][] queries = new int[Q][2];
+        for (int i = 0; i < Q; i++) {
+            st = new StringTokenizer(br.readLine());
+            queries[i][0] = Integer.parseInt(st.nextToken());
+            queries[i][1] = Integer.parseInt(st.nextToken());
         }
 
     
@@ -29,12 +45,8 @@ public class Main {
         }
 
 
-        int[][] queries = new int[Q][2];
         for (int i = 0; i < Q; i++) {
             int result;
-
-            queries[i][0] = sc.nextInt();
-            queries[i][1] = sc.nextInt();
             int decodeX = findLower(queries[i][0], tree, mapper);
             int decodeY = findUpper(queries[i][1], tree, mapper);
 
