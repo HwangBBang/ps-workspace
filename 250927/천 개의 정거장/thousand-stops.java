804f1349380debb3d4793692c0d2541d7a335e0e
BYEONGHUN HWANG <h1009218@hufs.ac.kr>
2025-09-27 14:38:07 +0900

    Title: 천 개의 정거장, Time: 94ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 250927/천 개의 정거장/thousand-stops.java
@@ -0,0 +1,142 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+
+    static class Node implements Comparable <Node>{
+        int num;
+        long cost;
+        int time;
+
+        public Node(int num,long cost, int time){
+            this.num = num;
+            this.cost = cost;
+        }
+        
+        @Override 
+        public int compareTo(Node other){
+            if (this.cost != other.cost)
+                return Long.compare(this.cost, other.cost);
+            else
+                return Integer.compare(this.time, other.time);
+        }
+    }
+
+    static class Edge{
+        int to;
+        int fare;
+        int time;
+
+        public Edge(int to,int fare, int time){
+            this.to = to;
+            this.fare = fare;
+            this.time = time;
+        }
+    }
+
+    static List<Edge>[] graph;
+    
+    static long[] minCost;
+    static int[] minTime;
+
+    static final long INF = Long.MAX_VALUE;
+    static final int NODE_CNT = 1000;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st; 
+
+        st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        
+        graph = new ArrayList[NODE_CNT+1];
+
+        for (int i = 1; i <= NODE_CNT; i++){
+            graph[i] = new ArrayList<>();
+        }
+
+        minCost = new long[NODE_CNT+1];
+        minTime = new int[NODE_CNT+1];
+        Arrays.fill(minCost, INF);
+        Arrays.fill(minTime, Integer.MAX_VALUE);
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int cost = Integer.parseInt(st.nextToken());
+            int stopCount = Integer.parseInt(st.nextToken());
+            
+            int [] path = new int[stopCount];
+
+            st = new StringTokenizer(br.readLine());
+
+            for (int j = 0; j < stopCount; j++) {
+                int stop = Integer.parseInt(st.nextToken());
+                path[j] = stop;
+            }
+
+            for (int u = 0; u < stopCount; u++){
+                for (int v = u+1; v < stopCount; v++){
+                    graph[path[u]].add(new Edge(path[v],cost,v-u));
+                }
+            }
+        }
+
+
+        dijkstra(A);
+
+        StringBuilder sb = new StringBuilder();
+        if (minCost[B] == INF) sb = sb.append(-1).append(" ").append(-1);
+        else sb = sb.append(minCost[B]).append(" ").append(minTime[B]);        
+        System.out.println(sb);
+        
+    }
+
+    static void dijkstra(int start){
+        minCost[start] = 0;
+        minTime[start] = 0;
+        PriorityQueue<Node> minHeap = new PriorityQueue<>();
+        minHeap.add(new Node(start,0, 0));
+
+        while(!minHeap.isEmpty()){
+            Node curNode = minHeap.poll();
+
+            if (curNode.cost != minCost[curNode.num] || curNode.time != minTime[curNode.num]) continue;
+
+            for (Edge edge : graph[curNode.num]){
+                long newCost = edge.fare + curNode.cost; 
+                int newTime = edge.time + curNode.time; 
+
+                if (minCost[edge.to] > newCost){
+                    minCost[edge.to] = newCost;
+                    minTime[edge.to] = newTime;
+                    minHeap.add(new Node(edge.to, newCost, newTime));
+                } else if (minCost[edge.to] == newCost && minTime[edge.to] > newTime){
+                    minTime[edge.to] = newTime;
+                    minHeap.add(new Node(edge.to, newCost, newTime));
+                }
+
+            }
+        }
+    }
+}
+
+/*
+    1000 개 노드 (정점)
+    N개의 버스가 이동 각 노선 -> 다익스트라 여러번..?
+    
+    각 버스는 출발지 -> 도착지 ,다시 출발지 -> 도착지
+    탑승료 (가중치)
+
+    중간 하차시 - 탑승료 동일 
+    노선 하나가 하나의 큰 Edge
+    도착지 도달시 반드시 하차 
+    
+    최소 비용을 위한 최소 시간 구하기
+    "최소 비용"이 관리 포인트 
+    
+    1_000_000_000
+    
+*/ 
