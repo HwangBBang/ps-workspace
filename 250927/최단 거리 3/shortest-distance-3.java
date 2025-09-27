7897e2d9cfc8eded29aaa537b2d21639f3618424
BYEONGHUN HWANG <h1009218@hufs.ac.kr>
2025-09-27 12:16:17 +0900

    Title: 최단 거리 3, Time: 300ms, Memory: 22MB, Status: Passed - Codetree

--- 250927/최단 거리 3/shortest-distance-3.java
+++ 250927/최단 거리 3/shortest-distance-3.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.io.*;
 
 
 public class Main {
@@ -16,7 +17,6 @@ public class Main {
         public int compareTo(Node other){
             return Integer.compare(this.dist, other.dist);
         }
-
     }
 
     static class Edge{
@@ -33,10 +33,16 @@ public class Main {
     static int[] dist;
     static final int INF = Integer.MAX_VALUE;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
         graph = new ArrayList[n+1]; 
         for (int i = 1; i <= n; i++) 
@@ -47,16 +53,17 @@ public class Main {
 
 
         for (int i = 0; i < m; i++) {
-            int u = sc.nextInt();
-            int v = sc.nextInt();
-            int w = sc.nextInt();
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
 
             graph[u].add(new Edge(v,w));
             graph[v].add(new Edge(u,w));
         }
-
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+        st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
 
         dijkstra(a);
 
@@ -70,8 +77,9 @@ public class Main {
 
         while(!minHeap.isEmpty()){
             Node curNode = minHeap.poll();
-
-            if(curNode.dist != dist[curNode.num]) continue;
+            
+            if(curNode.dist != dist[curNode.num]) continue; 
+            
 
             for (Edge edge : graph[curNode.num]){
                 int newDist = edge.weight + dist[curNode.num];
@@ -91,5 +99,7 @@ public class Main {
     1 ~ N 까지 노드 
     M개의 간선 + 가중치 + 양방향
     A -> B 까지의 최단거리 
+    dist[] 에는 최선의 기록만 담긴다. 
+    dist[num]과 현재 노드에 기록된 dist 가 다르다면, 이웃 노드 갱신 작업이 의미가 없다. 
 
 */
