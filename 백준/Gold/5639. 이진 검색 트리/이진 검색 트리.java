f27544fbaca31920e7e654e0ad237a4cc5947cf5
BYEONGHUN HWANG <h1009218@hufs.ac.kr>
2025-10-14 15:05:59 +0900

    [Gold IV] Title: 이진 검색 트리, Time: 260 ms, Memory: 18892 KB -BaekjoonHub

--- 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.java
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.java
@@ -1,21 +1,19 @@
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     static ArrayList<Integer> preOrder;
-    static ArrayList<Integer> postOrder;
+    static StringBuilder postOrder;
+
     public static void main(String[] args) throws IOException {
         // System.setIn(new FileInputStream("src/baekjoon/gold/input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String line;
+        postOrder = new StringBuilder();
 
         preOrder = new ArrayList<>();
         preOrder.add(0);
-        postOrder = new ArrayList<>();
 
 
         while ((line = br.readLine()) != null) {
@@ -23,14 +21,12 @@ public class Main {
             preOrder.add(num);
         }
         solution(1, preOrder.size() - 1);
-
-        for (Integer each : postOrder) {
-            System.out.println(each);
-        }
+        
+        System.out.println(postOrder);
 
     }
 
-    static int solution(int start, int end) {
+    static void solution(int start, int end) {
 
         int rootValue = preOrder.get(start);
 
@@ -52,8 +48,8 @@ public class Main {
         if (rightSubStartIdx <= rightSubEndIdx) {
             solution(rightSubStartIdx, rightSubEndIdx);
         }
-        postOrder.add(rootValue);
-        return rootValue;
+
+        postOrder.append(rootValue).append("\n");
     }
 
 }
