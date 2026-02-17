20ea0f208df02d210ea9296c4ea29eea64451d07
BYEONGHUN HWANG <h1009218@hufs.ac.kr>
2026-02-17 21:59:12 +0900

    [Silver V] Title: 스네이크버드, Time: 116 ms, Memory: 14440 KB -BaekjoonHub

---
+++ 백준/Silver/16435. 스네이크버드/스네이크버드.java
@@ -0,0 +1,32 @@
+// package baekjoon.silver;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+//        System.setIn(new FileInputStream("src/baekjoon/silver/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int l = Integer.parseInt(st.nextToken());
+        int[] nums = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(nums);
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > l) break;
+            l ++;
+        }
+        System.out.println(l);
+
+    }
+}
